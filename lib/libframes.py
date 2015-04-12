e8494fa2c2a05e3dbbc02c49fba958572fe0aa56
David Hall <david63025@gmail.com>
2015-04-11 22:41:10 -0500

    added adaptive threshold

--- lib/libframes.py
+++ lib/libframes.py
@@ -14,7 +14,7 @@
 
 __filename__ = "libframes.py"
 __author__ = "David Hall"
-__last_update__ = "05 April 2015"
+__last_update__ = "11 April 2015"
 
 import os
 import sys
@@ -26,7 +26,7 @@ def simple_threshold(image, threshold, maxval, th_type):
     """Implements simple thresholding algorithm from OpenCV
 
     Arguments:
-        image: image to perform threshold
+        image: image to threshold
         threshold: threshold value
                    > 0 and less than largest pixel value
         maxval: value of output pixel if image pixel is greater than threshold value
@@ -36,10 +36,30 @@ def simple_threshold(image, threshold, maxval, th_type):
                          cv2.THRESH_TRUNC,
                          cv2.THRESH_TOZERO, cv2.THRESH_TOZERO_INV
     Returns a list:
-        threshold value; integer
-        thresholded image; numpy array"""
+        threshold value: integer
+        thresholded image: numpy array"""
     ret, th_img = cv2.threshold(image, threshold, maxval, th_type)
     return th_img
+
+def adaptive_threshold(image, maxval, ad_method, th_type, blk_size, constant):
+    """Implements adaptive thresholding algorithm from OpenCV
+
+    Arguments:
+        image: source image
+        maxval: value of output pixel if image pixel is greater than threshold value
+                range of values: 0 -> largest integer
+        ad_method: threshold algorithm
+                   accepts cv2.ADAPTIVE_THRESH_MEAN_C,
+                           cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+        th_type: threshold type
+                 accepts cv2.THRESH_BINARY, cv2.THRESH_BINARY_INV
+        blk_size: size of pixel neighborhood used to calculate threshol level
+                  odd integer > 0 (3, 5, 7, etc.)
+        constant: subtracted from mean
+                  normally a postive integer
+    Returns a list:
+        thresholded image: numpy array"""
+    return cv2.adaptiveThreshold(image, maxval, ad_method, th_type, blk_size, constant)
 
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
@@ -62,5 +82,16 @@ if __name__ == "__main__":
         plt.imshow(img)
         plt.axis('off')
         plt.title('Original')
+        plt.show()
 
+    for (title, method) in list((('Adaptive Mean', cv2.ADAPTIVE_THRESH_MEAN_C),
+                                 ('Adaptive Gaussian', cv2.ADAPTIVE_THRESH_GAUSSIAN_C))):
+        plt.subplot(2, 1, 1)
+        plt.imshow(adaptive_threshold(img[..., 0], white, method, cv2.THRESH_BINARY, 11, 2))
+        plt.axis('off')
+        plt.title(title)
+        plt.subplot(2, 1, 2)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title('Original')
         plt.show()
